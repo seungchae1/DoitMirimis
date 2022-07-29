6495f5692a4478314a21641b545bdc8c57b6f18c
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-29 15:40:06 +0900

    Add Activity

---
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/GetCharacterActivity1.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2125.doitmirimis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetCharacterActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_character1);
+    }
+}
---
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/GetCharacterActivity2.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2125.doitmirimis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetCharacterActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_character2);
+    }
+}
