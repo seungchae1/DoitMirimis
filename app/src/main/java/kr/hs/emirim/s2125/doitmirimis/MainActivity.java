cdbadedc8bc5ba2480f8e0246b4f27520c42e89c
seungchae1 <s2125@e-mirim.hs.kr>
2022-07-29 14:37:17 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2125.doitmirimis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
