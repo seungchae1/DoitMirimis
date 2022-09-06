5a94bc25305333edc38b9a5e883efa0b127bd2ff
xxx-1203 <s2119@e-mirim.hs.kr>
2022-09-06 19:05:40 +0900

    button change

--- app/src/main/java/kr/hs/emirim/s2125/doitmirimis/GetCharacterActivity2.java
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/GetCharacterActivity2.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.s2125.doitmirimis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class GetCharacterActivity2 extends AppCompatActivity {
 
@@ -10,5 +13,26 @@ public class GetCharacterActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_character2);
+        ImageButton check_btn = findViewById(R.id.check_btn);
+        ImageButton character_btn = findViewById(R.id.character_btn);
+        check_btn.setOnClickListener(btnListener);
+        character_btn.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = null;
+            switch (view.getId()){
+                case R.id.check_btn:
+                    intent = new Intent(GetCharacterActivity2.this, MainActivity.class);
+                    GetCharacterActivity2(intent);
+                    break;
+                case R.id.character_btn:
+                    intent = new Intent(GetCharacterActivity2.this, MainActivity.class);
+                    GetCharacterActivity2(intent);
+                    break;
+            }
+        }
+    };
 }
