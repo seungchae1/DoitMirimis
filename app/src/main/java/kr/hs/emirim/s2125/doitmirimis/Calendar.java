30eb1e084a4aa7282609efe9d379fbfdc85aa72d
sinheungheung <s2120@e-mirim.hs.kr>
2022-09-22 19:21:41 +0900

    Calendar 완성(확인 전)

--- app/src/main/java/kr/hs/emirim/s2125/doitmirimis/Calendar.java
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/Calendar.java
@@ -1,14 +1,76 @@
 package kr.hs.emirim.s2125.doitmirimis;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
-public class Calendar extends AppCompatActivity {
+class activity_calendar extends AppCompatActivity {
 
+    Calendar myCalendar = Calendar.getInstance();
+
+    DatePickerDialog.OnDateSetListener myDatePicker = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, month);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+    };
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        EditText et_Date = (EditText) findViewById(R.id.Date);
+        et_Date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(activity_calendar.this, myDatePicker, myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    final EditText et_time = (EditText)  findViewById(R.id.Time);
+    et_time.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY) + 9; // 9를 더하여 한국 시간으로 변경함
+
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+            TimePickerDialog mTimePicker;
+            mTimePicker = new TimePickerDialog(activity_calendar.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    String state = "AM";
+                    // 선택할 시간이 12시를 넘을 경우 "PM"으로 변경 및 -12시간하여 출력
+                    if (selectedHour > 12) {
+                        selectedHour -= 12;
+                        state = "PM";
+                    }
+                    et_time.setText(state + " " + selectedHour + "시 " + selectedMinute + "분");
+                }
+            }, hour, minute, false); // true의 경우 24시간 형식의 TimePicker 출현
+            mTimePicker.setTitle("Select Time");
+            mTimePicker.show();
+        }
+    });
+    }
+    private void updateLabel(){
+        String myFormat = "yyyy/MM/dd";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.KOREA);
+
+        EditText et_date = (EditText) findViewById(R.id.Date);
+        et_date.setText(sdf.format(myCalendar.getTime()));
     }
 }
+
+    
