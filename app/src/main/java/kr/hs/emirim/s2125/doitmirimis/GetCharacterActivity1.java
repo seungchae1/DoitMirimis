e06e376bc1a1ccc794fc11a765a568ae2b97dc58
xxx-1203 <s2119@e-mirim.hs.kr>
2022-09-27 18:48:41 +0900

    캐릭터 획득 페이지 수정

--- app/src/main/java/kr/hs/emirim/s2125/doitmirimis/GetCharacterActivity1.java
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/GetCharacterActivity1.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.s2125.doitmirimis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class GetCharacterActivity1 extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class GetCharacterActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_character1);
+        ImageButton box_btn = findViewById(R.id.box_btn);
+        box_btn.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = null;
+            switch (view.getId()){
+                case R.id.box_btn:
+                    intent = new Intent(GetCharacterActivity1.this, GetCharacterActivity2.class);
+                    startActivity(intent);
+                    break;
+            }
+        }
+    };
 }
