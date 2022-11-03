1a2b5eb0557c175297769541b17e04196ab1c5db
seungchae1 <s2125@e-mirim.hs.kr>
2022-11-03 20:06:49 +0900

    checkbox로 바꿔보기

--- app/src/main/java/kr/hs/emirim/s2125/doitmirimis/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.hs.emirim.s2125.doitmirimis;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.icu.util.MeasureUnit;
 import android.os.Bundle;
@@ -9,13 +12,18 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     LinearLayout frame;
+    TextView text1;
+    EditText edit1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +35,12 @@ public class MainActivity extends AppCompatActivity {
         btnCalendar.setOnClickListener(btnListener);
         ImageButton btnPlus = findViewById(R.id.plus);
         btnPlus.setOnClickListener(btnListener);
+        CheckBox check1 = findViewById(R.id.check1);
+        check1.setOnClickListener(btnListener);
+        text1= findViewById(R.id.text1);
+        text1.setOnClickListener(btnListener);
+
+        edit1=findViewById(R.id.ch_edit);
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
@@ -44,15 +58,69 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     break;
                 case R.id.plus:
-                    EditText newEdit = new EditText(MainActivity.this);
+                    View checkView = View.inflate(getApplicationContext(), R.layout.new_checkbox, null);
+                    TextView checkVText = checkView.findViewById(R.id.text1);
+                    checkVText.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                            View dlgView = View.inflate(MainActivity.this, R.layout.checkbox_dlg,null);
+                            EditText editDlg = dlgView.findViewById(R.id.ch_edit);
+                            dlg.setView(dlgView);
+                            dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                            dlg.show();
+                        }
+                    });
+                    CheckBox check1 = checkView.findViewById(R.id.check1);
+                    frame.addView(checkView);
+
+                    /*CheckBox newEdit = new CheckBox(MainActivity.this);
+                    TextView newText = new TextView(MainActivity.this);
                     FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 250);
+                    newText.setLayoutParams(params);
                     newEdit.setLayoutParams(params);
-                    newEdit.setBackgroundResource(R.drawable.rectangle1);
-                    newEdit.setGravity(Gravity.TOP);
-                    newEdit.setHint("오늘의 할 일은 무엇인가요?");
-                    newEdit.setTextSize(15);
-                    newEdit.setPadding(80,87,100,100);
-                    frame.addView(newEdit);
+                    newText.setBackgroundResource(R.drawable.rectangle1);
+                    newText.setHint("오늘의 할 일은 무엇인가요?");
+                    newText.setTextSize(15);
+                    newText.setPadding(100,80,0,0);
+                    newEdit.setPadding(10,0,0,0);
+
+                    newText.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                            View dlgView = View.inflate(MainActivity.this, R.layout.checkbox_dlg,null);
+                            EditText editDlg = dlgView.findViewById(R.id.ch_edit);
+                            dlg.setView(dlgView);
+                            dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+
+                                }
+                            });
+                            dlg.show();
+                        }
+                    });
+                    frame.addView(newText);
+                    frame.addView(newEdit);*/
+                case R.id.text1:
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    View dlgView = View.inflate(MainActivity.this, R.layout.checkbox_dlg,null);
+                    EditText editDlg = dlgView.findViewById(R.id.ch_edit);
+                    editDlg.setText(text1.getText());
+                    dlg.setView(dlgView);
+                    dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            text1.setText(editDlg.getText());
+                        }
+                    });
+                    dlg.show();
             }
         }
     };
---
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/checkbox_dlg.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2125.doitmirimis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class checkbox_dlg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.checkbox_dlg);
+    }
+}
