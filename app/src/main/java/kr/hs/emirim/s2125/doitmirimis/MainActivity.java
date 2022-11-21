b2e090f7ed61a32d9ad13020bf4eef0210f36673
seungchae1 <s2125@e-mirim.hs.kr>
2022-11-22 08:39:09 +0900

    SQLite로 checkbox내용 저장

--- app/src/main/java/kr/hs/emirim/s2125/doitmirimis/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2125/doitmirimis/MainActivity.java
@@ -1,11 +1,13 @@
 package kr.hs.emirim.s2125.doitmirimis;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.icu.util.MeasureUnit;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -19,17 +21,47 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+    static RequestQueue requestQueue;
     LinearLayout frame;
     TextView text1;
     EditText edit1;
     ArrayList<CheckBox> checkList = new ArrayList<CheckBox>();
+    int cnt;
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 타이틀바 없애는 거임!! 지우지 마셈!!
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        sharedPreferences= getSharedPreferences("test", MODE_PRIVATE);    // test 이름의 기본모드 설정, 만약 test key값이 있다면 해당 값을 불러옴.
+        editor = sharedPreferences.edit(); //sharedPreferences를 제어할 editor를 선언
+
         setContentView(R.layout.activity_main);
         frame = findViewById(R.id.frame);
         ImageButton btnCharacter = findViewById(R.id.btn_char);
@@ -42,13 +74,39 @@ public class MainActivity extends AppCompatActivity {
         check1.setOnClickListener(btnListener);
         text1= findViewById(R.id.text1);
         text1.setOnClickListener(btnListener);
+        text1.setText(sharedPreferences.getString("default",""));
 
-        edit1=findViewById(R.id.ch_edit);
+        cnt=sharedPreferences.getInt("cnt",0);
+        for(int i=0; i<cnt; i++){
+            View checkView = View.inflate(getApplicationContext(), R.layout.new_checkbox, null);
+            TextView checkVText = checkView.findViewById(R.id.text_n);
+            checkVText.setText(sharedPreferences.getString(Integer.toString(i),""));
+            checkVText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    View dlgView = View.inflate(MainActivity.this, R.layout.checkbox_dlg,null);
+                    EditText editDlg = dlgView.findViewById(R.id.ch_edit);
+                    editDlg.setText(checkVText.getText());
+                    dlg.setView(dlgView);
+                    dlg.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            checkVText.setText(editDlg.getText());
+                        }
+                    });
+                    dlg.show();
+                }
+            });
+            CheckBox check = checkView.findViewById(R.id.check_n);
+            frame.addView(checkView);
+        }
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+
             Intent intent = null;
             switch (view.getId()) {
                 case R.id.btn_calendar:
@@ -61,6 +119,9 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(intent);
                     break;
                 case R.id.plus:
+                    cnt++;
+                    editor.putInt("cnt",cnt);
+                    editor.commit();
                     View checkView = View.inflate(getApplicationContext(), R.layout.new_checkbox, null);
                     TextView checkVText = checkView.findViewById(R.id.text_n);
                     checkVText.setOnClickListener(new View.OnClickListener() {
@@ -75,6 +136,8 @@ public class MainActivity extends AppCompatActivity {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
                                     checkVText.setText(editDlg.getText());
+                                    editor.putString( Integer.toString(cnt-1),editDlg.getText().toString());
+                                    editor.commit();
                                 }
                             });
                             dlg.show();
@@ -93,6 +156,8 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
                             text1.setText(editDlg.getText());
+                            editor.putString( "default",editDlg.getText().toString());
+                            editor.commit();
                         }
                     });
                     dlg.show();
